d1231ee42d072fea182b277e3d9b0830a41de13e
Lina Ben-Younes <115539807+lina-by@users.noreply.github.com>
2024-09-30 23:22:12 +0200

    Column number fix

--- format_data.py
+++ format_data.py
@@ -1,47 +1,9 @@
 import numpy as np
 import pandas as pd
 
-NUMERICAL_COL = [
-    "Carbon concentration / (weight%)", 
-    "Silicon concentration / (weight%)", 
-    "Manganese concentration / (weight%)",
-    "Sulphur concentration / (weight%)", 
-    "Phosphorus concentration / (weight%)", 
-    "Nickel concentration / (weight%)",
-    "Chromium concentration / (weight%)", 
-    "Molybdenum concentration / (weight%)", 
-    "Vanadium concentration / (weight%)",
-    "Copper concentration / (weight%)", 
-    "Cobalt concentration / (weight%)", 
-    "Tungsten concentration / (weight%)",
-    "Oxygen concentration / parts per million by weight", 
-    "Titanium concentration / parts per million by weight",
-    "Nitrogen concentration / parts per million by weight", 
-    "Aluminium concentration / parts per million by weight",
-    "Boron concentration / parts per million by weight", 
-    "Niobium concentration / parts per million by weight",
-    "Tin concentration / parts per million by weight", 
-    "Arsenic concentration / parts per million by weight",
-    "Antimony concentration / parts per million by weight", 
-    "Current / A", 
-    "Voltage / V", 
-    "Heat input / kJmm-1", 
-    "Interpass temperature / °C",
-    "Post weld heat treatment temperature / °C", 
-    "Post weld heat treatment time / hours", 
-    "Yield strength / MPa", 
-    "Ultimate tensile strength / MPa",
-    "Elongation / %", 
-    "Reduction of Area / %", 
-    "Charpy temperature / °C", 
-    "Charpy impact toughness / J", 
-    "Hardness / kgmm-2",
-    "Primary ferrite in microstructure / %", 
-    "Ferrite with second phase / %", 
-    "Acicular ferrite / %", 
-    "Martensite / %", 
-    "Ferrite with carbide aggreagate / %"
-]
+COLUMNS = ["Carbon concentration / (weight%)", "Silicon concentration / (weight%)", "Manganese concentration / (weight%)", "Sulphur concentration / (weight%)", "Phosphorus concentration / (weight%)", "Nickel concentration / (weight%)", "Chromium concentration / (weight%)", "Molybdenum concentration / (weight%)", "Vanadium concentration / (weight%)", "Copper concentration / (weight%)", "Cobalt concentration / (weight%)", "Tungsten concentration / (weight%)", "Oxygen concentration / parts per million by weight", "Titanium concentration / parts per million by weight", "Nitrogen concentration / parts per million by weight", "Aluminium concentration / parts per million by weight", "Boron concentration / parts per million by weight", "Niobium concentration / parts per million by weight", "Tin concentration / parts per million by weight", "Arsenic concentration / parts per million by weight", "Antimony concentration / parts per million by weight", "Current / A", "Voltage / V", "AC or DC", "Electrode positive or negative", "Heat input / kJmm-1", "Interpass temperature / °C", "Type of weld", "Post weld heat treatment temperature / °C", "Post weld heat treatment time / hours", "Yield strength / MPa", "Ultimate tensile strength / MPa", "Elongation / %", "Reduction of Area / %", "Charpy temperature / °C", "Charpy impact toughness / J", "Hardness / kgmm-2", "50% FATT", "Primary ferrite in microstructure / %", "Ferrite with second phase / %", "Acicular ferrite / %", "Martensite / %", "Ferrite with carbide aggreagate / %", "Weld ID"]
+
+
 
 CATEGORICAL_COL = [
     "AC or DC", 
@@ -50,8 +12,9 @@ CATEGORICAL_COL = [
     "50% FATT", 
     "Weld ID"
 ]
+NUMERICAL_COL = list(set(COLUMNS) - set(CATEGORICAL_COL))
 
-COLUMNS = NUMERICAL_COL+CATEGORICAL_COL
+
 
 def convert_less_than(value):
     if isinstance(value, str) and value.startswith('<'):
