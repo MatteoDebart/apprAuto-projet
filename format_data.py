c447b3dda425417178aadf3049b227064bb12e1a
Bouhadida Malek <malek.bouhadida@student-cs.fr>
2024-10-01 18:49:23 +0200

    small code refacto

--- format_data.py
+++ format_data.py
@@ -1,29 +1,12 @@
-import numpy as np
 import pandas as pd
+from utils import convert_less_than
+
 
 COLUMNS = ["Carbon concentration / (weight%)", "Silicon concentration / (weight%)", "Manganese concentration / (weight%)", "Sulphur concentration / (weight%)", "Phosphorus concentration / (weight%)", "Nickel concentration / (weight%)", "Chromium concentration / (weight%)", "Molybdenum concentration / (weight%)", "Vanadium concentration / (weight%)", "Copper concentration / (weight%)", "Cobalt concentration / (weight%)", "Tungsten concentration / (weight%)", "Oxygen concentration / parts per million by weight", "Titanium concentration / parts per million by weight", "Nitrogen concentration / parts per million by weight", "Aluminium concentration / parts per million by weight", "Boron concentration / parts per million by weight", "Niobium concentration / parts per million by weight", "Tin concentration / parts per million by weight", "Arsenic concentration / parts per million by weight", "Antimony concentration / parts per million by weight", "Current / A", "Voltage / V", "AC or DC", "Electrode positive or negative", "Heat input / kJmm-1", "Interpass temperature / °C", "Type of weld", "Post weld heat treatment temperature / °C", "Post weld heat treatment time / hours", "Yield strength / MPa", "Ultimate tensile strength / MPa", "Elongation / %", "Reduction of Area / %", "Charpy temperature / °C", "Charpy impact toughness / J", "Hardness / kgmm-2", "50% FATT", "Primary ferrite in microstructure / %", "Ferrite with second phase / %", "Acicular ferrite / %", "Martensite / %", "Ferrite with carbide aggreagate / %", "Weld ID"]
 
+CATEGORICAL_COL = ["AC or DC", "Electrode positive or negative", "Type of weld", "Weld ID"]
 
-
-CATEGORICAL_COL = [
-    "AC or DC", 
-    "Electrode positive or negative", 
-    "Type of weld", 
-    "50% FATT", 
-    "Weld ID"
-]
 NUMERICAL_COL = list(set(COLUMNS) - set(CATEGORICAL_COL))
-
-
-
-def convert_less_than(value):
-    if isinstance(value, str) and value.startswith('<'):
-        try:
-            number = float(value[1:])
-            return np.random.uniform(0, number)
-        except ValueError:
-            return value
-    return value
 
 
 def create_dataframe(file_path):
@@ -38,5 +21,6 @@ def create_dataframe(file_path):
     return db
 
 if __name__=='__main__':
+
     file_path="welddb/welddb.data"
     create_dataframe(file_path)
--- utils.py
+++ utils.py
@@ -1,6 +1,19 @@
 import pandas as pd
+import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
+
+
+def convert_less_than(value):
+    """Converts less-than instances with real numbers respecting the condition."""
+    
+    if isinstance(value, str) and value.startswith('<'):
+        try:
+            number = float(value[1:])
+            return np.random.uniform(0, number)
+        except ValueError:
+            return value
+    return value
 
 
 def plot_distribution(column):
