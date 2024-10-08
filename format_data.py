f6095598fd1d52918c6272cf7b36dad924598317
MatteoDebart <m4tteo.debart@gmail.com>
2024-10-08 16:59:12 +0200

    include outliers handling in preprocess function + fix arg issue for OneHotEncoder

--- format_data.py
+++ format_data.py
@@ -4,13 +4,14 @@ import numpy as np
 
 from sklearn.preprocessing import OneHotEncoder
 
-COLUMNS = ["Carbon concentration / (weight%)", "Silicon concentration / (weight%)", "Manganese concentration / (weight%)", "Sulphur concentration / (weight%)", "Phosphorus concentration / (weight%)", "Nickel concentration / (weight%)", "Chromium concentration / (weight%)", "Molybdenum concentration / (weight%)", "Vanadium concentration / (weight%)", "Copper concentration / (weight%)", "Cobalt concentration / (weight%)", "Tungsten concentration / (weight%)", "Oxygen concentration / parts per million by weight", "Titanium concentration / parts per million by weight", "Nitrogen concentration / parts per million by weight", "Aluminium concentration / parts per million by weight", "Boron concentration / parts per million by weight", "Niobium concentration / parts per million by weight", "Tin concentration / parts per million by weight", "Arsenic concentration / parts per million by weight", "Antimony concentration / parts per million by weight", "Current / A", "Voltage / V", "AC or DC", "Electrode positive or negative", "Heat input / kJmm-1", "Interpass temperature / °C", "Type of weld", "Post weld heat treatment temperature / °C", "Post weld heat treatment time / hours", "Yield strength / MPa", "Ultimate tensile strength / MPa", "Elongation / %", "Reduction of Area / %", "Charpy temperature / °C", "Charpy impact toughness / J", "Hardness / kgmm-2", "50% FATT", "Primary ferrite in microstructure / %", "Ferrite with second phase / %", "Acicular ferrite / %", "Martensite / %", "Ferrite with carbide aggreagate / %", "Weld ID"]
+COLUMNS = ["Carbon concentration / (weight%)", "Silicon concentration / (weight%)", "Manganese concentration / (weight%)", "Sulphur concentration / (weight%)", "Phosphorus concentration / (weight%)", "Nickel concentration / (weight%)", "Chromium concentration / (weight%)", "Molybdenum concentration / (weight%)", "Vanadium concentration / (weight%)", "Copper concentration / (weight%)", "Cobalt concentration / (weight%)", "Tungsten concentration / (weight%)", "Oxygen concentration / parts per million by weight", "Titanium concentration / parts per million by weight", "Nitrogen concentration / parts per million by weight", "Aluminium concentration / parts per million by weight", "Boron concentration / parts per million by weight", "Niobium concentration / parts per million by weight",
+           "Tin concentration / parts per million by weight", "Arsenic concentration / parts per million by weight", "Antimony concentration / parts per million by weight", "Current / A", "Voltage / V", "AC or DC", "Electrode positive or negative", "Heat input / kJmm-1", "Interpass temperature / °C", "Type of weld", "Post weld heat treatment temperature / °C", "Post weld heat treatment time / hours", "Yield strength / MPa", "Ultimate tensile strength / MPa", "Elongation / %", "Reduction of Area / %", "Charpy temperature / °C", "Charpy impact toughness / J", "Hardness / kgmm-2", "50% FATT", "Primary ferrite in microstructure / %", "Ferrite with second phase / %", "Acicular ferrite / %", "Martensite / %", "Ferrite with carbide aggreagate / %", "Weld ID"]
 
 
 CATEGORICAL_COL = [
-    "AC or DC", 
-    "Electrode positive or negative", 
-    "Type of weld", 
+    "AC or DC",
+    "Electrode positive or negative",
+    "Type of weld",
     "Weld ID"
 ]
 
@@ -32,21 +33,24 @@ MECHANICAL_PROPERTIES = [
 
 NUMERICAL_COL = list(set(COLUMNS) - set(CATEGORICAL_COL))
 
-def AC_DC(x:Optional[str]):
-    if x=='AC':
+
+def AC_DC(x: Optional[str]):
+    if x == 'AC':
         return 0
-    if x=='DC':
+    if x == 'DC':
         return 1
     return None
 
-def electrode(x:Optional[str]):
-    if x=='0':
+
+def electrode(x: Optional[str]):
+    if x == '0':
         return 0
-    if x=='+':
+    if x == '+':
         return 1
-    if x=='-':
+    if x == '-':
         return -1
     return None
+
 
 def convert_less_than(value):
     if isinstance(value, str) and value.startswith('<'):
@@ -57,19 +61,22 @@ def convert_less_than(value):
             return value
     return value
 
-def create_dataframe(file_path:str):
-    Db = pd.read_csv(file_path, delimiter = "\s+", names=COLUMNS, na_values='N')
+
+def create_dataframe(file_path: str = "welddb/welddb.data"):
+    Db = pd.read_csv(file_path, delimiter="\s+", names=COLUMNS, na_values='N')
     Db[NUMERICAL_COL] = Db[NUMERICAL_COL].applymap(convert_less_than)
     Db[NUMERICAL_COL] = Db[NUMERICAL_COL].apply(pd.to_numeric, errors='coerce')
 
     # Handling categorical
     Db["AC or DC"] = Db["AC or DC"].apply(AC_DC)
-    Db["Electrode positive or negative"] = Db["Electrode positive or negative"].apply(electrode)
-    
-    encoder = OneHotEncoder(sparse=False)
+    Db["Electrode positive or negative"] = Db["Electrode positive or negative"].apply(
+        electrode)
+
+    encoder = OneHotEncoder(sparse_output=False)
     encoded_weld_type = encoder.fit_transform(Db[["Type of weld"]])
-    encoded_weld_df = pd.DataFrame(encoded_weld_type, columns=encoder.get_feature_names_out(['Type of weld']))
-    
+    encoded_weld_df = pd.DataFrame(
+        encoded_weld_type, columns=encoder.get_feature_names_out(['Type of weld']))
+
     # Concatenate the encoded one-hot columns with the original dataframe
     Db = pd.concat([Db, encoded_weld_df], axis=1)
 
@@ -78,7 +85,8 @@ def create_dataframe(file_path:str):
     Db.to_csv("table.csv")
     return Db
 
-if __name__=='__main__':
 
-    file_path="welddb/welddb.data"
+if __name__ == '__main__':
+
+    file_path = "welddb/welddb.data"
     create_dataframe(file_path)
